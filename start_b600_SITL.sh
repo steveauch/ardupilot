a7954d941c1721d9b61c0046f87f3d8720faabcd
Brandon Blake <bskblake@gmail.com>
2019-09-28 09:46:19 +0930

    Extend start_b600_SITL.sh
    * Can start from anywhere provided shell sets BASH_SOURCE appropriately
    * Can add arguments to pass to sim_vehicle.py

--- start_b600_SITL.sh
+++ start_b600_SITL.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-./Tools/autotest/sim_vehicle.py -v ArduPlane --aircraft ./ArduPlane/B-600 -f jsbsim:B-600 -L AUGC
+cd "${BASH_SOURCE%/*}/" || exit
+pwd
+./Tools/autotest/sim_vehicle.py -v ArduPlane --aircraft ./ArduPlane/B-600 -f jsbsim:B-600 -L AUGC $@
