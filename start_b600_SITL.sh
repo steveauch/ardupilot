4ec2450d085b4772a3d659ff6b2eaf5b47cef102
Brandon Blake <bskblake@gmail.com>
2020-01-17 12:32:33 +1030

    Add mission planner IP to sim script

--- start_b600_SITL.sh
+++ start_b600_SITL.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+MISSION_PLANNER_IP=192.168.0.75:14550 
 cd "${BASH_SOURCE%/*}/" || exit
 pwd
-./Tools/autotest/sim_vehicle.py -v ArduPlane --aircraft ./ArduPlane/B-600 -f jsbsim:B-600 -L AUGC $@
+./Tools/autotest/sim_vehicle.py -v ArduPlane --aircraft ./ArduPlane/B-600 -f \
+jsbsim:B-600 -L AUGC $@ -m --out=udp:$MISSION_PLANNER_IP 
